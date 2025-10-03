46001f1934b865b4b1d6d4f9a8535da7ef9bc236
Duarte <duarte.desousa.contato@gmail.com>
2025-10-03 11:37:08 -0300

    first commit

---
+++ FCAutoSave(Duarte).bat
@@ -0,0 +1,10 @@
+start https://github.com/?locale=pt-br
+git init
+git status
+git config --global user.name "Duarte"
+git config --global user.email duarte.desousa.contato@gmail.com
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/DuartedeSousa/aula06.git
+git push -u origin main
